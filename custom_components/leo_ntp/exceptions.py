f0d7ad9f39128435ea742221683454aea0b43086
David Cumps <david@cumps.be>
2023-05-26 13:30:13 +0200

    fix: add docstrings

--- custom_components/leo_ntp/exceptions.py
+++ custom_components/leo_ntp/exceptions.py
@@ -3,23 +3,28 @@
 
 class LeoNTPException(Exception):
     """Base class for all exceptions raised by LeoNTP."""
+
     pass
 
 
 class LeoNTPServiceException(Exception):
     """Raised when service is not available."""
+
     pass
 
 class NotAuthenticatedException(Exception):
     """Raised when session is invalid."""
+
     pass
 
 
 class GatewayTimeoutException(LeoNTPServiceException):
     """Raised when server times out."""
+
     pass
 
 
 class BadGatewayException(LeoNTPServiceException):
     """Raised when server returns Bad Gateway."""
+
     pass
